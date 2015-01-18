8e989ab5a6ec258400ceb100f45adfab6e7c9650
くすは <dj.kusuha+github@gmail.com>
2015-01-19 01:31:32 +0900

    * Add SingletonMonoBehaviour class. * Add test script for SingletonMonoBehaviour
    /Assets/Tests is exist for unity debuging, testing, and more.

--- Assets/Scripts/Utility/SingletonMonoBehaviour.cs
+++ Assets/Scripts/Utility/SingletonMonoBehaviour.cs
@@ -1,25 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// MonoBehaviourのシングルトンクラス
+/// </summary>
+/// <typeparam name="T">シングルトンにしたいMonoBehaviourクラス</typeparam>
 public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
 
+    /// <summary>
+    /// インスタンス変数
+    /// </summary>
     private static T _instance;
+
+    /// <summary>
+    /// インスタンスを取得
+    /// </summary>
     public static T Instance {
         get {
-            if( _instance != null ) {
+            if( _instance == null ) {
+                // シーン上に無いか検索
                 _instance = FindObjectOfType<T>();
                 if( _instance == null ) {
-                    Debug.LogWarning( typeof( T ) + " is nothing." );
+                    // 無い場合は生成する
+                    var obj = new GameObject( typeof( T ).ToString() );
+                    _instance = obj.AddComponent<T>();
+
+                    Debug.Log( "Create " + typeof( T ).ToString() );
                 }
             }
             return _instance;
         }
     }
 
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
     virtual protected void Awake() {
         CheckInstance();
     }
 
+    /// <summary>
+    /// インスタンスの存在チェック
+    /// </summary>
+    /// <returns>存在していない場合 true</returns>
     protected bool CheckInstance() {
         if( _instance == null ) {
             _instance = (T)this;
---
+++ Assets/Tests/Test.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        var soundManager = SoundManager.Instance;
+        Debug.Log( soundManager );
+    }
+}
