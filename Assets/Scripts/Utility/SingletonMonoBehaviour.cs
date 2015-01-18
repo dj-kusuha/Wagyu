99fda5ad4db5f9a434774e4b4c9c5707e34b934b
くすは <dj.kusuha+github@gmail.com>
2015-01-19 01:19:09 +0900

    * Change unity project folder path. * Add ignore rules for UnityVS

---
+++ Assets/Scripts/Utility/SingletonMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
+
+    private static T _instance;
+    public static T Instance {
+        get {
+            if( _instance != null ) {
+                _instance = FindObjectOfType<T>();
+                if( _instance == null ) {
+                    Debug.LogWarning( typeof( T ) + " is nothing." );
+                }
+            }
+            return _instance;
+        }
+    }
+
+    virtual protected void Awake() {
+        CheckInstance();
+    }
+
+    protected bool CheckInstance() {
+        if( _instance == null ) {
+            _instance = (T)this;
+            return true;
+        } else if( Instance == this ) {
+            return true;
+        }
+
+        Destroy( this );
+        return false;
+    }
+}
