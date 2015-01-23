1b5ea3879a2f5fbcb3b9877bdf35ad555621302f
くすは <dj.kusuha+github@gmail.com>
2015-01-24 03:10:55 +0900

    リファクタリング

--- Assets/Scripts/Kusuha/GameManager.cs
+++ Assets/Scripts/Kusuha/GameManager.cs
@@ -5,26 +5,87 @@ using System.Collections;
 /// ゲームのマネージャクラス
 /// </summary>
 public class GameManager : SingletonMonoBehaviour<GameManager> {
+    //-------------------------------------------------------------------------
+    #region // Inspectorで設定するprivate変数
 
-    
-    private GameObject _player;
-    private GameObject Player {
-        get {
-            if( this._player == null ) {
-                this._player = GameObject.FindWithTag( "Player" );
-            }
-            return this._player;
+    /// <summary>
+    /// プレイヤーの初期ヒットポイント
+    /// </summary>
+    [SerializeField, Tooltip( "プレイヤーの初期ヒットポイント" )]
+    private int PlayerStartHP;
+
+    #endregion
+    //-------------------------------------------------------------------------
+    #region // private変数
+
+    /// <summary>
+    /// 経過時間
+    /// </summary>
+    private float time;
+
+    /// <summary>
+    /// プレイヤーの現在のヒットポイント
+    /// </summary>
+    private int hp;
+
+    /// <summary>
+    /// プレイヤー(ユニティちゃん)のGameObject
+    /// </summary>
+    private GameObject player;
+
+    #endregion
+    //-------------------------------------------------------------------------
+    #region // privateプロパティ
+
+
+
+    #endregion
+    //-------------------------------------------------------------------------
+    #region // public変数
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    /// <remarks>ゲーム開始時に必ず呼ぶ</remarks>
+    public void Initialize() {
+        this.time = 0f;
+        this.hp = this.PlayerStartHP;
+        this.player = GameObject.FindWithTag( "Player" );
+    }
+
+    /// <summary>
+    /// ダメージを食らう処理
+    /// </summary>
+    public void OnDamage( int damage ) {
+        Debug.Log( "OnDamage" );
+
+        // ヒットポイントを減らす
+        this.hp -= damage;
+        // ヒットポイントが0以下になったら終わり
+        if( this.hp <= 0 ) {
+            // 今はとりあえずユニティちゃんを削除しておく
+            Destroy( this.player );
         }
     }
 
-    public void OnHitEnemy() {
-        Debug.Log( "OnHitEnemy" );
-
-        Destroy( this );
-        Destroy( this.Player );
-    }
-
+    /// <summary>
+    /// ゴールに当たった時
+    /// </summary>
     public void OnHitGoal() {
-        Debug.Log( "OnHitEnemy" );
+        Debug.Log( "OnHitGoal" );
     }
+
+    #endregion
+    //-------------------------------------------------------------------------
+    #region // private関数
+
+    /// <summary>
+    /// 開始処理
+    /// </summary>
+    private void Start() {
+        Initialize();
+    }
+
+    #endregion
+    //-------------------------------------------------------------------------
 }
--- Assets/Scripts/Kusuha/OnCollisionEnterChecker.cs
+++ Assets/Scripts/Kusuha/OnCollisionEnterChecker.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
+/// <summary>
+/// コリジョン判定クラス
+/// </summary>
 public class OnCollisionEnterChecker : MonoBehaviour {
     
     // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider (Since v1.0)
@@ -11,7 +13,7 @@ public class OnCollisionEnterChecker : MonoBehaviour {
         switch( collision.gameObject.tag ) {
             // 敵と当たった
             case "Enemy":
-                GameManager.Instance.OnHitEnemy();
+                GameManager.Instance.OnDamage( 1 );
                 break;
             // ゴールと当たった
             case "Goal":
