617906e6c8854f5fa6e00410d7e6a5a81a4d52e0
くすは <dj.kusuha+github@gmail.com>
2015-01-24 02:57:37 +0900

    プロジェクト整理

---
+++ Assets/Scripts/Kusuha/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ゲームのマネージャクラス
+/// </summary>
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+
+    [SerializeField]
+    private GameObject unityChan;
+
+    public void OnHitEnemy() {
+        Debug.Log( "OnHitEnemy" );
+
+        Destroy( this );
+        Destroy( this.unityChan );
+    }
+
+    public void OnHitGoal() {
+        Debug.Log( "OnHitEnemy" );
+    }
+}
---
+++ Assets/Scripts/Kusuha/OnCollisionEnterChecker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class OnCollisionEnterChecker : MonoBehaviour {
+    
+    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider (Since v1.0)
+    private void OnCollisionEnter( Collision collision ) {
+        Debug.Log( "OnCollisionEnter: " + collision.gameObject.name );
+
+        switch( collision.gameObject.tag ) {
+            // 敵と当たった
+            case "Enemy":
+                GameManager.Instance.OnHitEnemy();
+                break;
+            // ゴールと当たった
+            case "Goal":
+                GameManager.Instance.OnHitGoal();
+                break;
+            default:
+                break;
+        }
+    }
+
+}
