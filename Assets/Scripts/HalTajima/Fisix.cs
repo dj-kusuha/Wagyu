1eeb9ef5f19f08a616532df315f743dde9c9ab88
MasahiroTaji <taji@crypton.co.jp>
2015-01-24 02:04:05 +0900

    綿毛

---
+++ Assets/Scripts/HalTajima/Fisix.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fisix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rigidbody.AddForce (Vector3.up * 50,	ForceMode.Acceleration);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime);
+		}
+		rigidbody.AddForce (Vector3.up * Random.Range(0.0f, 12.0f),ForceMode.Acceleration);
+//		rigidbody.AddForce (Vector3.left * Random.Range(-50.0f, 50.0f),ForceMode.Acceleration);
+		rigidbody.AddForce (transform.forward * Random.Range(5.0f, 25.0f),ForceMode.Acceleration);
+	}
+}
